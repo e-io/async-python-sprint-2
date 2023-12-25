1f911243c356d00ed980b40ce14d09f3161fb331
Pavel <pmikhailov@wiley.com>
2023-12-25 23:29:45 +0200

    Logger is added

---
+++ logger.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def get_custom_logger():
+    """Default logger for all files in this project"""
+
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger('Default')
+    logger.setLevel(level='DEBUG')
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = get_custom_logger()
