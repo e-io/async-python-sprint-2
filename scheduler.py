a261eb101c9b0d8da638c978d3806f2d0d0ef3ce
Pavel <pmikhailov@wiley.com>
2023-12-28 13:49:37 +0200

    Very basic Scheduler.

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,45 @@
-class Scheduler:
-    def __init__(self, pool_size=10):
-        pass
+from configparser import ConfigParser
+from time import sleep
 
-    def schedule(self, task):
-        pass
+from job import Job
+from logger import logger
+from types import Request, Response, ResponseStatus
+
+
+class Scheduler:
+    def __init__(self, pool_size: int=10) -> None:
+        self.__pool_size: list[Job] = pool_size
+        self.__pending: list[Job] = []
+        self.__pool: list[Job] = []
+        self.__ready: list[Job] = []
+
+        config = ConfigParser()
+        config.read('setup.cfg')
+        self.__tick = float(config['scheduler']['tick'])
+
+    def schedule(self, task: Job) -> None:
+        self.__pending.append(task)
 
     def run(self):
-        pass
+        while True:
+            sleep(self.__tick)
+            space = self.__pool_size - len(self.__pool)  # must be >=0
+            if space and self.__pending:
+                task = self.__pending.pop(0)
+                task.run()
+                self.__pool.append(task)
+            if not self.__pool:
+                continue  # so, it will sleep for a __tick again
+            finish: list[int] = []
+            for (i, task) in enumerate(self.__pool):
+                task.send(Request.status)
+                response: Response = next(task)
+                logger.debug(response)
+                if response.status is ResponseStatus.finish:
+                    finish.append(i)
+            for i in finish:
+                task = self.__pool.pop(i)
+                self.__ready.append(i)
 
     def restart(self):
         pass
---
+++ types.py
@@ -0,0 +1,19 @@
+from enum import Enum
+from dataclasses import dataclass, field
+
+
+class Request(Enum):
+    status = 'Report status'
+
+
+class ResponseStatus(Enum):
+    progress = 'I am in progress'
+    result = 'Some results more are ready'
+    # result_finish = 'Result is ready. I am finished'
+    finish = 'I am finished'
+
+
+@dataclass
+class Response:
+    status = ResponseStatus
+    new_results: dict[int, ...] = field(default_factory=lambda: {})
