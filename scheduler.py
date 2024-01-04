707d63f3aca3f465bbcbc4204495d0577a9b4169
Pavel <pmikhailov@wiley.com>
2024-01-04 13:22:17 +0200

    The wrap for _Scheduler is created. Now Scheduler creates a process with _Scheduler. And the stop is been able to be called.

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from multiprocessing import Process, Queue
 from time import sleep
 
 from job import Job
@@ -11,6 +12,37 @@ from customtypes import (
 
 
 class Scheduler:
+    """
+    The 'official' simple wrap for the class _Scheduler
+    """
+    def __init__(self, pool_size: int = 10) -> None:
+        config = ConfigParser()
+        config.read('setup.cfg')
+        self.__tick = float(config['scheduler']['tick'])
+
+        self.scheduler = _Scheduler(pool_size=pool_size, tick=self.__tick)
+        self.process = None
+        self.queue = None
+
+    def schedule(self, job: Job) -> None:
+        """Add a job in the list of pending jobs."""
+        self.scheduler.schedule(job)
+
+    def run(self) -> None:
+        scheduler = self.scheduler
+        self.queue = Queue()
+        queue = self.queue
+        self.process = Process(target=scheduler.run, args=(queue,))
+        self.process.start()
+        # sleep(10 * self.__tick)
+        # self.process.join()
+
+    def stop(self) -> None:
+        logger.debug("Scheduler.stop is called")
+        self.queue.put('stop')
+
+
+class _Scheduler:
     """
     A class used to represent a Scheduler.
     It may contain several jobs inside.
@@ -26,27 +58,26 @@ class Scheduler:
     __tick : float
         something like 'a frequency' of the whole project in seconds
     """
-    def __init__(self, pool_size: int = 10) -> None:
+    def __init__(self, pool_size: int, tick: float) -> None:
         self.__pool_size: int = pool_size
         self.__pending: list[Job] = []
         self.__pool: list[Job] = []
         self.__ready: list[Job] = []
-
-        config = ConfigParser()
-        config.read('setup.cfg')
-        self.__tick = float(config['scheduler']['tick'])
+        self.__tick: float = tick
+        self.queue = None
 
     def schedule(self, job: Job) -> None:
         """Add a job in the list of pending jobs."""
         self.__pending.append(job)
 
-    def run(self) -> None:
+    def run(self, queue: Queue) -> None:
+        self.queue = queue
         self.__run()
 
     def stop(self) -> None:
         """Stop all jobs and backup their condition."""
-        logger.debug("Event 'safeword' is set")
-        ...
+        logger.debug("This is _Scheduler.stop()")
+        self.__backup()
         exit(0)
 
     def __run(self) -> None:
@@ -54,9 +85,6 @@ class Scheduler:
         while True:
             sleep(self.__tick)
             logger.debug("The first line of main 'while' cycle of Scheduler.")
-            # if event:
-            #   self.__backup()
-            #   exit(0)  # It's shown explicitly that the current thread finishes its work
 
             space = self.__pool_size - len(self.__pool)  # must be >=0
 
@@ -74,6 +102,16 @@ class Scheduler:
 
             finished: list[int] = []  # indices of finished jobs
             for (i, job) in enumerate(self.__pool):
+                sleep(self.__tick / 4)
+                if not self.queue.empty():
+                    message = self.queue.get()
+                    self.queue.put(message)
+                    if message == 'stop':
+                        self.stop()
+                        break
+                    else:
+                        pass  # just ignore
+
                 logger.debug(f"A call of 'next' for {job.get_id()}")
                 next(job.loop)
                 logger.debug(f"Sending request to {job.get_id()}")
@@ -105,6 +143,7 @@ class Scheduler:
         """A stub"""
         logger.debug("This is '__backup' function'")
         sleep(self.__tick)
+        ...  # saving the state of all "jobs" in a CSV file
 
     def __restore(self) -> None:
         """A stub"""
